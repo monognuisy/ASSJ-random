4845275fc8d82cebdee1de5b649fa6380e004ed4
monognuisy <ysmsmart1@gmail.com>
2024-02-28 10:55:16 +0900

    fix: eslint for enforcing styles

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,36 +1,32 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export type Description = {
-    relatedTopics: string[];
+  relatedTopics: string[];
 };
 export const Description = ({ relatedTopics }: Description) => {
-    const hashtagLine = fetchRelatedTopics(relatedTopics);
-    return (
-        <DescriptionWrapper>
-            <Hashtags>{hashtagLine}</Hashtags>
-        </DescriptionWrapper>
-    )
-}
+  const hashtagLine = fetchRelatedTopics(relatedTopics);
+  return (
+    <DescriptionWrapper>
+      <Hashtags>{hashtagLine}</Hashtags>
+    </DescriptionWrapper>
+  );
+};
 
 const fetchRelatedTopics = (relatedTopics: string[]) => {
-    /*
-      relatedTopics: The list of hashtags - only the words are provided and is already trimmed
+  /*
+    relatedTopics: The list of hashtags - only the words are provided and is already trimmed
 
-      returns: JSX expression for hashtags
-    */
-    const hashtags = relatedTopics.map(
-      relatedTopic => (
-        `# ${relatedTopic}　　`
-      )
-    );
-    return hashtags
+    returns: JSX expression for hashtags
+  */
+  const hashtags = relatedTopics.map((relatedTopic) => `# ${relatedTopic} `);
+  return hashtags;
 };
 
 const DescriptionWrapper = styled.div`
-    margin: 0;
-`
+  margin: 0;
+`;
 
 // This styled-component exists because of further styling.
 const Hashtags = styled.h4`
-    margin: 0;
-`
+  margin: 0;
+`;
